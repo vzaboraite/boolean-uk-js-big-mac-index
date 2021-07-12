aeaf5495a3fa53a4f1566fd6548ba98eb12e512c
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-12 17:38:57 +0100

    Calculate Big Mac Index of 5 countries.

--- index.js
+++ index.js
@@ -22,9 +22,58 @@ For example, using figures in July 2008:
 // Big Mac Index by the example above
 const basePrice = 3.57; //USD
 const priceGBP = 2.29;
-let ppp = basePrice / priceGBP; // here ppp - purchasing power parity
+let ippGBP = basePrice / priceGBP; // here ipp - implied purchasing power
+const actualExchangeRateGBP = 2;
+const valueGBP =
+  ((actualExchangeRateGBP - ippGBP.toFixed(2)) / ippGBP.toFixed(2)) * 100; // percents
+console.log(valueGBP.toFixed() + "%"); // 28%
 
-let actualExchangeRateGBP = 2;
-let valueGBP =
-  ((actualExchangeRateGBP - ppp.toFixed(2)) / ppp.toFixed(2)) * 100; // percents
-console.log(parseInt(valueGBP));
+// Big Mac Index of 5 other countries today
+const basePriceUSD = 5.66;
+
+// Lithuania
+const priceLTEur = 2.8;
+const ippLTEur = basePriceUSD / priceLTEur;
+const actualCurrencyExchangeRateEur = 1.19; // €1 = $1.19
+const valueLTEur =
+  ((actualCurrencyExchangeRateEur - ippLTEur.toFixed(2)) /
+    ippLTEur.toFixed(2)) *
+  100;
+console.log(valueLTEur.toFixed() + "%"); // -41%
+
+// United Kingdom
+const priceUKGBP = 3.19;
+const ippUKGBP = basePriceUSD / priceUKGBP;
+const actualCurrencyExchangeRateGBP = 1.39; // £1 = $1.39
+const valueUKGBP =
+  ((actualCurrencyExchangeRateGBP - ippUKGBP.toFixed(2)) /
+    ippUKGBP.toFixed(2)) *
+  100;
+console.log(valueUKGBP.toFixed() + "%"); // -21%
+
+// Switzerland
+const priceCHF = 6.5;
+const ippCHF = basePriceUSD / priceCHF;
+const actualCurrencyExchangeRateCHF = 1.09; // 1 swiss frank = $1.09
+const valueCHF =
+  ((actualCurrencyExchangeRateCHF - ippCHF.toFixed(2)) / ippCHF.toFixed(2)) *
+  100;
+console.log(valueCHF.toFixed() + "%"); // 25%
+
+// Ukraine
+const priceUAH = 62;
+const ippUAH = basePriceUSD / priceUAH;
+const actualCurrencyExchangeRateUAH = 0.037; // 1UAH = $0.037
+const valueUAH =
+  ((actualCurrencyExchangeRateUAH - ippUAH.toFixed(2)) / ippUAH.toFixed(2)) *
+  100;
+console.log(valueUAH.toFixed() + "%"); // -59%
+
+// Norway
+const priceNOK = 52;
+const ippNOK = basePriceUSD / priceNOK;
+const actualCurrencyExchangeRateNOK = 0.11; // 1NOK = $0.11
+const valueNOK =
+  ((actualCurrencyExchangeRateNOK - ippNOK.toFixed(2)) / ippNOK.toFixed(2)) *
+  100;
+console.log(valueNOK.toFixed() + "%"); // 0%
